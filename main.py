7125a09e040a4766b00f8e95d6f6aeb918b68675
louispaulet <louispaulet13@gmail.com>
2023-03-24 20:33:47 +0100

    add working bootstrap and custom css file

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 from typing import List
 from transformers import pipeline, AutoTokenizer, AutoModelForTokenClassification
@@ -10,6 +11,8 @@ import re
 logging.basicConfig(level=logging.INFO)
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 templates = Jinja2Templates(directory="templates")
 
